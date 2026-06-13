2ea0f7f3552d3ff829221d189ecd9588f8119fc9
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-06-13 16:08:15 +0530

    Deparment

--- departments/models.py
+++ departments/models.py
@@ -4,9 +4,13 @@ from django.db import models
 
 class Department(models.Model):
 
-     departments=(
-          
-          ('Roads&Infrastructure','Roads&Infrastructure'),
-          ('WaterWorks','WaterWorks'),
-          ()
-     )
+    dept_id=models.CharField(max_length=17,unique=True)
+    dept_name=models.CharField(max_length=100)
+    descrption=models.TextField(blank=True)
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.dept_name
+    
+    
+    
